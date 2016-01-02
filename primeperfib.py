11af177f44c3d6855c68280a260b8bbabdf8fcf1
Sébastien Dawans <sebastien@dawans.be>
2016-01-02 21:52:36 +0100

    Apply PEP8 function naming conventions

--- primeperfib.py
+++ primeperfib.py
@@ -8,7 +8,7 @@ class PrimePerFib:
     def __init__(self):
         pass
   
-    def Factor(self, n):
+    def factor(self, n):
         yield 1
         i = 2
         limit = n**0.5
@@ -22,8 +22,8 @@ class PrimePerFib:
         if n > 1:
             yield n
             
-    def PrimeFactor(self, n):
-        for x in self.Factor(n):
+    def prime_factor(self, n):
+        for x in self.factor(n):
             if x != 1 and x != n:
                 return False
         return True
@@ -32,7 +32,7 @@ class PrimePerFib:
     A Hybrid Sieve of Atkin, without all the quadratics and keeping of sequences
     allows for generating blocks of primes
     """
-    def PrimeGen(self, count, start):
+    def prime_gen(self, count, start):
         c     = 0
         base  = [2,3,5]
         start = int(round(start))
@@ -48,30 +48,30 @@ class PrimePerFib:
                 
             if not [m for m in base if (n%60) % m == 0 ]:
                 if n % n**0.5 != 0:
-                    if self.PrimeFactor(n):
+                    if self.prime_factor(n):
                         c += 1
                         yield n
             n += 2
                     
-    def PrimeGet(self, num):
-        r = self.PrimeGen(1,num).next()
+    def prime_get(self, num):
+        r = self.prime_gen(1,num).next()
         return r
     
     
-    def IsPrime(self, num):
-        return (False,True)[self.PrimeGet(num)==num]
+    def is_prime(self, num):
+        return (False,True)[self.prime_get(num)==num]
     
-    def NextPrime(self, num):
-        return (self.PrimeGet(num),self.PrimeGet(num+1))[self.IsPrime(num)]
+    def prime_next(self, num):
+        return (self.prime_get(num),self.prime_get(num+1))[self.is_prime(num)]
 
-    def PerfectGen(self, count, start=0):
+    def perfect_gen(self, count, start=0):
         output = 0
         prime  = 0 
         while output < count:
-            prime = self.NextPrime(prime)
+            prime = self.prime_next(prime)
             mPrime = 2**prime - 1
 
-            if not self.IsPrime(mPrime):
+            if not self.is_prime(mPrime):
                 continue
             
             pN =(2**(prime-1))*(2**prime - 1)
@@ -79,16 +79,16 @@ class PrimePerFib:
                 output += 1
                 yield pN
                     
-    def PerfectGet(self, num):
-        return self.PerfectGen(1,num).next()
+    def perfect_get(self, num):
+        return self.perfect_gen(1,num).next()
     
-    def IsPerfect(self, num):
-        return (False,True)[self.PerfectGet(num)==num]
+    def is_perfect(self, num):
+        return (False,True)[self.perfect_get(num)==num]
     
-    def NextPerfect(self, num):
-        return (self.PerfectGet(num),self.PerfectGet(num+1))[self.IsPerfect(num)]
+    def perfect_next(self, num):
+        return (self.perfect_get(num),self.perfect_get(num+1))[self.is_perfect(num)]
     
-    def FibonacciGen(self, count, start=0):
+    def fibonacci_gen(self, count, start=0):
         output = 0
         fib    = [0,1]
         while output < count:
@@ -99,14 +99,14 @@ class PrimePerFib:
                 output += 1
                 yield fN
         
-    def FibonacciGet(self, num):
-        return self.FibonacciGen(1,num).next()
+    def fibonacci_get(self, num):
+        return self.fibonacci_gen(1,num).next()
 
-    def IsFibonacci(self, num):
-        return (False,True)[self.FibonacciGet(num)==num]
+    def is_fibonacci(self, num):
+        return (False,True)[self.fibonacci_get(num)==num]
     
-    def NextFibonacci(self, num):
-        return (self.FibonacciGet(num),self.FibonacciGet(num+1))[self.IsFibonacci(num)]
+    def fibonacci_next(self, num):
+        return (self.fibonacci_get(num),self.fibonacci_get(num+1))[self.is_fibonacci(num)]
 
 if __name__ == '__main__':
     
@@ -115,11 +115,11 @@ if __name__ == '__main__':
     ##########################################################################
     #Prime Numbers
     
-    print "What Prime Number Comes After 56? ",PPF.NextPrime(56)
-    print "Is 333 A Prime Number? ",PPF.IsPrime(333)
+    print "What Prime Number Comes After 56? ",PPF.prime_next(56)
+    print "Is 333 A Prime Number? ",PPF.is_prime(333)
 
     Primes = []
-    for Prime in PPF.PrimeGen(10,42):
+    for Prime in PPF.prime_gen(10,42):
         Primes.append(Prime)
         
     print "Generated Primes: ",Primes,"\n\n"
@@ -128,11 +128,11 @@ if __name__ == '__main__':
     #Perfect Numbers
     #Need Some Horsepower In Your Machine To Play With These
     
-    print "What Perfect Number Comes After 9685 ",PPF.NextPerfect(9685)
-    print "Is 8128 A Perfect Number ",PPF.IsPerfect(8128)
+    print "What Perfect Number Comes After 9685 ",PPF.perfect_next(9685)
+    print "Is 8128 A Perfect Number ",PPF.is_perfect(8128)
     
     Perfects = []
-    for Perfect in PPF.PerfectGen(8, 0):
+    for Perfect in PPF.perfect_gen(8, 0):
         Perfects.append(Perfect)
         
     print "Generated Perfect Numbers ",Perfects,"\n\n"
@@ -140,11 +140,11 @@ if __name__ == '__main__':
     ##########################################################################
     #Fibonacci Numbers
     
-    print "What is the Next Fibonacci Number After 5 ? ",PPF.NextFibonacci(5)
-    print "Is 16 A Fibonacci Number? ",PPF.IsFibonacci(16)
+    print "What is the Next Fibonacci Number After 5 ? ",PPF.fibonacci_next(5)
+    print "Is 16 A Fibonacci Number? ",PPF.is_fibonacci(16)
     
     Fibonaccis = []
-    for Fibonacci in PPF.FibonacciGen(42, 10):
+    for Fibonacci in PPF.fibonacci_gen(42, 10):
         Fibonaccis.append(Fibonacci)
         
     print "Generated Fibonacci Numbers ",Fibonaccis
