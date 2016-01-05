f23a31d0c1098b3297c6ab938ef32695bfe302d4
jeroentbt <jeroen@tinktenk.be>
2016-01-05 21:32:38 +0100

    Fix script (called wrong function)

--- script_jeroentbt.py
+++ script_jeroentbt.py
@@ -18,5 +18,5 @@ args = parser.parse_args()
 change = cash_register.compute_change(args.due, args.paid)
 pprint.pprint(change)
 
-magic = calc.duh(args.due, args.paid)
+magic = calc.magic(args.due, args.paid)
 print("The magic number is %d" % magic)
