71204f62aae3d23aeb8150c28bffb2fb15cf5f1f
benhendrickx <benhendrickx1@hotmail.com>
2016-01-05 21:35:36 +0100

    Added bhendrickx function

--- script_bhendrickx.py
+++ script_bhendrickx.py
@@ -8,3 +8,8 @@ cash_register = CashRegister()
 
 change = cash_register.compute_change(140,210)
 print(change)
+
+a=10
+b=50
+result = calc.bhendrickx(a,b)
+print("Result of the bhendrickx(%d,%d) is %d" % (a, b, result))
